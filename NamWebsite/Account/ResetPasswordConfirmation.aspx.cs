423f0975c29ce378224142443f7ad16b5b81d6a4
nyoon1 <35244631+nyoon1@users.noreply.github.com>
2018-01-15 18:10:20 -0700

    CreateProject

---
+++ NamWebsite/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace NamWebsite.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ NamWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NamWebsite.Startup))]
+namespace NamWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
